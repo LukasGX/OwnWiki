18aa6cbb36e4a1f4fa6636a2b67b95513b42f746
LukasGX <lukas@grambs.info>
2025-08-07 13:44:39 +0200

    Templates and Site links fix

--- backend/include.php
+++ backend/include.php
@@ -28,10 +28,12 @@ function routing() {
         exit;
     }
 
-    $filename = strtolower(explode(":", $route)[1]);
-    $filepath = "pages/" . $filename . ".md";
+    $split = explode(":", $route);
+    $namespace = strtolower($split[0]);
+    $filename = strtolower($split[1]);
+    $filepath = "pages/$namespace/$filename.md";
 
-    $text = file_get_contents($filepath) ?? null;
+    $text = @file_get_contents($filepath) ?? null;
     if ($text === null) {
         header("HTTP/1.0 404 Not Found");
         exit;
@@ -41,13 +43,56 @@ function routing() {
 }
 
 function getHtml($args) {
-    $converter = new CommonMarkConverter();
-    return $converter->convertToHtml($args[1]);
+    $conv_config = [
+        'commonmark' => [
+            'unordered_list_markers' => ['-', '+'],
+        ],
+        'html_input' => 'allow',
+        'allow_unsafe_links' => false,
+        'max_nesting_level' => PHP_INT_MAX,
+    ];
+
+    $converter = new CommonMarkConverter($conv_config);
+
+    // own rules
+    $args[1] = preg_replace_callback('/{{\s*([A-Za-z0-9_]+)\s*}}/', function ($matches) {
+        $templateName = $matches[1];
+        $templatePath = 'pages/Template/' . strtolower($templateName) . '.md';
+        if (file_exists($templatePath)) {
+            return file_get_contents($templatePath);
+        } else {
+            return 'File not found: ' . htmlspecialchars($templatePath);
+        }
+    }, $args[1]);
+    // parse
+    $dirty_html = $converter->convertToHtml($args[1]);
+
+    $config = HTMLPurifier_Config::createDefault();
+    // Configuration for HTMLPurifier
+    $config->set('HTML.Allowed', 'div,i,h2,h3,h4,h5,h6,p,span,ul,ol,li,a,strong,em,br,img,table,tr,td,th');
+    $config->set('HTML.ForbiddenElements', ['b']);
+    $config->set('HTML.AllowedAttributes', '*.class,*.style, a.href, a.title, img.src, img.alt, img.title');
+    $config->set('CSS.AllowedProperties', [
+        'color',
+        'background-color',
+        'text-align',
+        'margin',
+        'padding',
+        'border'
+    ]);
+    $config->set('CSS.AllowImportant', false); // no !important
+    $purifier = new HTMLPurifier($config);
+
+    $clean_html = $purifier->purify($dirty_html);
+
+    echo $clean_html;
 }
 
 function getTitle($args) {
-    $filename = strtolower(explode(":", $args[0])[1]);
-    $filepath = "pages/" . $filename . ".json";
+    $split = explode(":", $args[0]);
+    $namespace = strtolower($split[0]);
+    $filename = strtolower($split[1]);
+    $filepath = "pages/$namespace/$filename.json";
 
     $text = @file_get_contents($filepath) ?? null;
     if ($text === false || $text === null) {
---
+++ pages/template/sla.md
@@ -0,0 +1,1 @@
+Schnelllöschantrag
